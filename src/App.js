4342fa98d3f372ebdb18df274a6637e410e85b6f
serginhohalves <80188778+serginhohalves@users.noreply.github.com>
2022-05-23 19:26:17 -0300

    mensagem

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+  class App extends React.Component {
+    // Component é  uma classe que herda de React.Component que é a classe mais importante do React
+    //lifecycle methods sao metodos que executam automaticamente quando o componente é renderizado 
+    state = {
+            counter: 0,
+            posts: [
+              {
+                id:1,
+                title: 'Post 1',
+                body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.' 
+              },
+              {
+                id:2,
+                title: 'Post 2',
+                body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.'
+              },
+              {
+                id:3,
+                title: 'Post 3',
+                body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.'
+              }
+            ]
+          };
+
+    
+          timeoutUpdate = null;
+      
+    componentDidMount(){ 
+      this.handleTimeout();
+    }
+
+    componentDidUpdate(){
+      this.handleTimeout();
+    }
+
+    componentWillUnmount(){
+      clearTimeout(this.timeoutUpdate);
+    }
+
+
+    handleTimeout = () => { // função que atualiza o estado
+      const{posts, counter} = this.state
+      posts[0].title = 'title changed';
+
+      this.timeoutUpdate =  setTimeout(()=>{
+        this.setState({
+          posts, counter: counter + 1
+        })
+      }, 1000)
+      
+    }  
+
+    render() {
+      const {posts, counter} = this.state
+      return (
+        
+        <div className="App">
+          <h1>{counter}</h1>
+          { 
+            posts.map(post => (
+              
+              <div key={post.id}>
+              <h1>{post.title}</h1>
+              <p>{post.body}</p>
+              </div>
+            ))
+          }
+   
+        </div>
+      );
+    }
+  }
 
 export default App;
