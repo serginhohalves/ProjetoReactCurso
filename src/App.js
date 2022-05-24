dcaa2760e96931f467df5cc69b7c4f528dcf0987
serginhohalves <80188778+serginhohalves@users.noreply.github.com>
2022-05-24 00:04:14 -0300

    Novo Commit - Chamadas Api

--- src/App.js
+++ src/App.js
@@ -1,78 +1,56 @@
-import './App.css';
-import React from 'react';
+import { Component } from "react";
 
+import "./App.css";
 
-  class App extends React.Component {
-    // Component é  uma classe que herda de React.Component que é a classe mais importante do React
-    //lifecycle methods sao metodos que executam automaticamente quando o componente é renderizado 
-    state = {
-            counter: 0,
-            posts: [
-              {
-                id:1,
-                title: 'Post 1',
-                body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.' 
-              },
-              {
-                id:2,
-                title: 'Post 2',
-                body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.'
-              },
-              {
-                id:3,
-                title: 'Post 3',
-                body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.'
-              }
-            ]
-          };
+class App extends Component {
+  // Component é  uma classe que herda de React.Component que é a classe mais importante do React
+  //lifecycle methods sao metodos que executam automaticamente quando o componente é renderizado
 
-    
-          timeoutUpdate = null;
-      
-    componentDidMount(){ 
-      this.handleTimeout();
-    }
+  // state é um objeto que armazena informações do componente
+  state = {
+    // estado inicial do componente
+    posts: [],
+  };
 
-    componentDidUpdate(){
-      this.handleTimeout();
-    }
-
-    componentWillUnmount(){
-      clearTimeout(this.timeoutUpdate);
-    }
-
-
-    handleTimeout = () => { // função que atualiza o estado
-      const{posts, counter} = this.state
-      posts[0].title = 'title changed';
-
-      this.timeoutUpdate =  setTimeout(()=>{
-        this.setState({
-          posts, counter: counter + 1
-        })
-      }, 1000)
-      
-    }  
-
-    render() {
-      const {posts, counter} = this.state
-      return (
-        
-        <div className="App">
-          <h1>{counter}</h1>
-          { 
-            posts.map(post => (
-              
-              <div key={post.id}>
-              <h1>{post.title}</h1>
-              <p>{post.body}</p>
-              </div>
-            ))
-          }
-   
-        </div>
-      );
-    }
+  //metodo que executa quando o componente é renderizado
+  componentDidMount() {
+    this.loadPosts();
   }
+
+  loadPosts = async () => {
+    const postResponse = fetch("https://jsonplaceholder.typicode.com/posts"); // faz uma requisição ao servidor
+    const photosResponse = fetch("https://jsonplaceholder.typicode.com/photos");
+
+    const [posts, photos] = await Promise.all([postResponse, photosResponse]); // retorna um array com os dados do postResponse
+  
+    const postJson = await posts.json(); // transforma o postResponse em json
+    const photosJson = await photos.json(); // transforma o photosResponse em json
+
+
+    const postAndPhotos = postJson.map((post,index)=>{
+      return{ ...post, cover: photosJson[index].url}
+    })
+    this.setState({ posts:postAndPhotos}); // atualiza o estado do componente
+  }; 
+
+  render() {
+    const { posts } = this.state;
+    return (
+      <section className="container">
+        <div className="posts">
+          {posts.map((post) => (
+            <div className="post">
+              <img src={post.cover} alt={post.title}/>
+              <div key={post.id} className="post-content">
+                <h1>{post.title}</h1>
+                <p>{post.body}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
+    );
+  }
+}
 
 export default App;
